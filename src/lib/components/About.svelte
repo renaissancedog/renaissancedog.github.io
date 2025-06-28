d63d9ac5820438910b2dee9205f974588cb5f0f8
William Liu <orangetnt729@gmail.com>
2025-06-28 14:29:44 -0500

    update things

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -8,14 +8,15 @@
       class="relative m-2 mb-2 flex min-h-[600px] flex-col justify-center rounded-2xl border-8 border-black bg-purple-300 px-6 py-8 text-lg font-semibold drop-shadow-2xl xl:mt-0 xl:w-1/3"
     >
       <p>
-        Hello! I’m William, a high school sophomore with a huge passion for
-        STEM. Whether it’s coding, building cool electronics, or diving into
-        fascinating branches of math, I love figuring out how things work.
-        <br />
-        I also enjoy competing in STEM competitions and connecting with others who
-        share my curiosity. When I’m not busy with school, you’ll probably find me
-        exercising, practicing my violin, creating something new, or dreaming up
-        my next big idea.
+        Hello! I’m William, a rising high school junior who loves to learn about
+        our world. I'm especially interested in math - my favorite subjects
+        being algebra and combinatorics - and I also like to create things, both
+        physically and digitally, to bring ideas to life.
+        <br /> <br />
+        Outside of school, you’ll probably find me practicing my violin, training
+        for a triathlon, reading, watching YouTube (3BlueBrown, Jay Foreman, Wendover,
+        and Jet Lag are my favorite channels), or spending time in nature. I'm always
+        looking for new ways to learn, create, and grow.
       </p>
     </div>
     <img
