658cd9d318a57ea13095ce32c4385b048cb345bd
William Liu <jiyangliu729@gmail.com>
2025-12-25 12:48:09 -0600

    bump

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -11,12 +11,12 @@
         Hello! I’m William, a high school junior who loves to learn about our
         world.
         <br /> <br />
-        I'm extremely interested in all types of mathematics. Beyond the typical
-        high-school curriculum, I enjoy studying all sorts of advanced math, such
-        as analysis, linear algebra, combinatorics, number theory, and probability.
-        I am currently preparing to help write a research paper (on scissors congruence
-        in higher dimensions) as part of Texas A&M's PReMa program. I have also qualified
-        for the AIME three times and won awards competing in various math competitions
+        I'm extremely interested in all types of mathematics. Beyond the typical high-school
+        curriculum, I enjoy studying all sorts of advanced math, such as analysis,
+        linear algebra, combinatorics, number theory, and probability. I am currently
+        preparing to help write a research paper (on scissors congruence in higher
+        dimensions) as part of Texas A&M's PReMa program. I have also qualified for
+        the AIME three times and won awards competing in various math competitions
         (such as the AMC series, the Stanford Math Tournament, and UIL.)
         <br /> <br />
         I like to create all types of things, both physically and digitally, to bring
@@ -29,16 +29,15 @@
         <br /> <br />
         I believe that knowledge is best used when shared, so I enjoy helping others
         learn. I have tutored peers in all sorts of STEM fields, and in my positions
-        at school (as an officer of the math team and the math honor society), I
-        work to promote and foster an appreciation of math, as well as of learning
-        in general.
+        at school (as an officer of the math team and the math honor society), I work
+        to promote and foster an appreciation of math, as well as of learning in general.
         <br /> <br />
         Outside of my adventures in STEM, you’ll probably find me practicing my violin,
         exercising (I'm running a half-marathon soon!), reading, watching YouTube
-        (3BlueBrown, Vsauce, Wendover, and Jet Lag are my favorite channels), or
-        spending time in nature. In the future, I hope to study something STEM-related
-        in college, and eventually become a scientist or engineer to create the ideas
-        that make our world a better place.
+        (3BlueBrown, Vsauce, Wendover, and Jet Lag are my favorite channels), or spending
+        time in nature. In the future, I hope to study something STEM-related in college,
+        and eventually become a scientist or engineer to create the ideas that make
+        our world a better place.
       </p>
     </div>
     <img
